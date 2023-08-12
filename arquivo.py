82978b9a94b142c6d502034216d7159dda7a1a1e
Dalva <dalva.mariana.dm@gmail.com>
2023-08-12 18:57:36 -0300

    teste: não tem a palavra esperada

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,6 @@
 def main():
     print("Este é um exemplo de código para a GitHub Action.")
-    print("Aqui está a palavra 'repositorio'.")
+    print("Aqui está a palavra 'banan'.")
 
 if __name__ == "__main__":
     main()

edc95487c7f115c680c55a55189a2b312406a1d8
Dalva <dalva.mariana.dm@gmail.com>
2023-08-13 11:21:05 -0300

    apagando a palavra certa

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,6 @@
 def main():
     print("Este é um exemplo de código para a GitHub Action.")
-    print("Aqui está a palavra 'banan'.")
+    print("Aqui está a palavra banana")
 
 if __name__ == "__main__":
     main()

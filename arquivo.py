2f1fff160a646f23ce295b1ae031552b343ea377
Dalva <dalva.mariana.dm@gmail.com>
2023-08-13 12:00:40 -0300

    teste: COM a palavra

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,6 @@
 def main():
     print("Este é um exemplo de código para a GitHub Action.")
-    print("Aqui está a palavra Repositorio")
+    print("Aqui está a palavra repositorio")
 
 if __name__ == "__main__":
     main()

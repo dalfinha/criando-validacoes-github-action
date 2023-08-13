a86859d237cd048e35724263edf49ed0194ec51c
Dalva <dalva.mariana.dm@gmail.com>
2023-08-13 12:21:47 -0300

    teste: atinge 2/3

--- arquivo.py
+++ arquivo.py
@@ -1,7 +1,7 @@
 def main():
     print("Este é um exemplo de código para a GitHub Action.")
-    print("Aqui está a palavra repositorio, servico_operacional, tecnologia")
-    #print("Aqui está a palavra banana, servico_operacional, tecnologia")
+    #print("Aqui está a palavra repositorio, servico_operacional, tecnologia")
+    print("Aqui está a palavra banana, servico_operacional, tecnologia")
     #print("Aqui está a palavra repositorio, teste, tecnologia")
     #print("Aqui está a palavra repositorio, servico_operacional, teste")
     #print("Aqui está a palavra repositorio, servico_operacional, tecnologia")
